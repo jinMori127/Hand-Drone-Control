759765041c7a390ec95f55dc75f490cde0518ccc
sami <sami19.serhan@gmail.com>
2025-01-16 13:42:12 +0200

    wp - code fixes

--- tello.py
+++ tello.py
@@ -40,9 +40,9 @@ if __name__ == "__main__":
     p_error = 0
 
     # Debounce variables
-    debounce_counts = {"mode": 0}  # Track how many consecutive frames a gesture is detected
+    debounce_counts = {"Mode": 0}  # Track how many consecutive frames a gesture is detected
     debounce_threshold = 10  # Minimum frames for stable recognition
-    mode_change_threshold = 20  # Minimum frames for stable mode change
+    mode_change_threshold = 10  # Minimum frames for stable mode change
     detected_gesture = None
     modes = ["Hand Detection", "Face Tracking", "Hand and Face Detection"]
 
@@ -55,7 +55,7 @@ if __name__ == "__main__":
 
         # Go up to the height of the person
         tello.send_rc_control(0, 0, 25, 0)
-        sleep(2.7)
+        sleep(2.2)
 
         while True:
             img = tello.get_frame_read().frame
@@ -84,27 +84,39 @@ if __name__ == "__main__":
 
             if results.multi_hand_landmarks and results.multi_handedness:
                 for hand_landmark, hand_handedness in zip(results.multi_hand_landmarks, results.multi_handedness):  # REMOVE FOR
-                    lm_list = []
-                    finger_fold_status = []
+                    # Process the data
                     used_hand = hand_handedness.classification[0].label  # Left or Right
+                    horizontal_fingers = []
+                    up_fingers = []
+                    lm_list = []
 
-                    # Extract each finger
+                    current_gesture = None
+
+
+                    '''
+                        Finger detection and folded state detection
+                    '''
+
                     for id, lm in enumerate(hand_landmark.landmark):
                         lm_list.append(lm)
 
-                    # Extract x,y of finger tips
+                    # Detect folded fingers when left hand is pointing right, used for Up and Down
                     for tip in finger_tips:
-                        x, y = int(lm_list[tip].x * w), int(lm_list[tip].y * h)
+                        horizontal_fingers.append(lm_list[tip].x < lm_list[tip - 2].x)
 
-                        if lm_list[tip].x < lm_list[tip - 2].x:
-                            finger_fold_status.append(True)
-                        else:
-                            finger_fold_status.append(False)
+                    # Detect folded fingers when hand is pointing up, used for Mode Change
+                    up_fingers.append(lm_list[4].x < lm_list[3].x and lm_list[3].x < lm_list[2].x and lm_list[2].x < lm_list[1].x) # thumb needs special case, uses X
+                    for tip in finger_tips:
+                        up_fingers.append(lm_list[tip].y < lm_list[tip-1].y and lm_list[tip-1].y < lm_list[tip-2].y and lm_list[tip-2].y < lm_list[tip-3].y)
 
-                    # Gesture recognition logic
-                    current_gesture = None
+                    up_thumb = lm_list[4].y < lm_list[3].y and lm_list[3].y < lm_list[2].y and lm_list[2].y < lm_list[1].y
+                    down_thumb = lm_list[4].y > lm_list[3].y and lm_list[3].y > lm_list[2].y and lm_list[2].y > lm_list[1].y
 
-                    # Stop
+
+                    '''
+                        Gesture processing
+                    '''
+
                     if lm_list[4].y < lm_list[2].y and lm_list[8].y < lm_list[6].y and lm_list[12].y < lm_list[10].y and \
                             lm_list[16].y < lm_list[14].y and lm_list[20].y < lm_list[18].y and lm_list[17].x < lm_list[
                         0].x < lm_list[5].x:
@@ -132,12 +144,8 @@ if __name__ == "__main__":
                             lm_list[16].x < lm_list[14].x and lm_list[20].x < lm_list[18].x:
                         current_gesture = "Left"  # drone's left
 
-                    up_thumb = lm_list[4].y < lm_list[3].y and lm_list[3].y < lm_list[2].y and lm_list[2].y < lm_list[
-                        1].y
-                    down_thumb = lm_list[4].y > lm_list[3].y and lm_list[3].y > lm_list[2].y and lm_list[2].y > lm_list[
-                        1].y
-
-                    if all(finger_fold_status):
+                    # Up - Down
+                    if all(horizontal_fingers):
 
                         if up_thumb:
                             current_gesture = "Up"
@@ -145,61 +153,58 @@ if __name__ == "__main__":
                         if down_thumb:
                             current_gesture = "Down"
 
-                    # Change mode
-                    up_finger1 = lm_list[4].x < lm_list[3].x and lm_list[3].x < lm_list[2].x and lm_list[2].x < lm_list[
-                        1].x
-                    up_finger2 = lm_list[8].y < lm_list[7].y and lm_list[7].y < lm_list[6].y and lm_list[6].y < lm_list[
-                        5].y
-                    up_finger3 = lm_list[12].y < lm_list[11].y and lm_list[11].y < lm_list[10].y and lm_list[10].y < \
-                                 lm_list[9].y
-                    up_finger4 = lm_list[16].y < lm_list[15].y and lm_list[15].y < lm_list[14].y and lm_list[14].y < \
-                                 lm_list[13].y
-                    up_finger5 = lm_list[20].y < lm_list[19].y and lm_list[19].y < lm_list[18].y and lm_list[18].y < \
-                                 lm_list[17].y
+                    # Mode
+                    if not up_fingers[0] and up_fingers[1] and not up_fingers[2] and not up_fingers[3] and up_fingers[4]:
+                        current_gesture = "Mode"
 
-                    up_fingers = [up_finger1, up_finger2, up_finger3, up_finger4, up_finger5]
-                    for i, finger in enumerate(up_fingers):
-                        if finger:
-                            print(f"Finger{i}")
 
-                    if not up_finger1 and up_finger2 and not up_finger3 and not up_finger4 and up_finger5:
-
-                        debounce_counts["mode"] += 1
-                        if debounce_counts["mode"] >= mode_change_threshold:
-                            current_mode = (current_mode + 1) % 3
-                            debounce_counts["mode"] = 0
+                    '''
+                        Command processing
+                    '''
 
                     # Debouncing logic
-                    if current_gesture:
+                    if current_gesture: # Increment the corresponding gesture count, decrement the rest
                         debounce_counts[current_gesture] = debounce_counts.get(current_gesture, 0) + 1
+                        if current_gesture == "Mode":
+                            print("Mode before dec", debounce_counts[current_gesture])
+
                         for gesture in list(debounce_counts):
                             if gesture != current_gesture:
                                 debounce_counts[gesture] = max(0, debounce_counts[gesture] - 1)
-                    else:
+
+                    else: # No gesture, decrement all the gesture counts
                         for gesture in list(debounce_counts):
-                            if gesture == "mode":
-                                continue
                             debounce_counts[gesture] = max(0, debounce_counts[gesture] - 1)
+                    if current_gesture == "Mode":
+                        print("Mode after dec", debounce_counts[current_gesture])
+                        print()
 
                     # Recognize gesture if it exceeds debounce threshold
                     for gesture, count in debounce_counts.items():
-                        if gesture == "mode":
-                            continue
-                        if count >= debounce_threshold:
+                        if gesture == "Mode" and count >= mode_change_threshold:
+                            print("Changing Mode")
+                            current_mode = (current_mode + 1) % 3
+                            debounce_counts["Mode"] = 0
                             detected_gesture = gesture
-                            # Remove 3 from counter for faster detection
+
+                        elif count >= debounce_threshold:
+                            detected_gesture = gesture
                             debounce_counts[gesture] = debounce_threshold - 1
 
+
+                    '''
+                        Add info to the screen
+                    '''
                     # Display recognized gesture
                     if detected_gesture:
-                        cv2.putText(img, detected_gesture, (20, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.8,
-                                    (0, 0, 255), 2)
+                        cv2.putText(img, detected_gesture, (20, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
 
                     mp_draw.draw_landmarks(img, hand_landmark,
                                            mp_hands.HAND_CONNECTIONS,
                                            mp_draw.DrawingSpec((0, 0, 255), 2, 2),
                                            mp_draw.DrawingSpec((0, 255, 0), 2, 2)
                                            )
+
 
             cv2.imshow("Hand Sign Detection", img)
             if cv2.waitKey(1) & 0xFF == ord('q'):  # Exit on 'q'
